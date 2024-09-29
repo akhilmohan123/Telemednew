7181ea9b9e3022d2608cb4acca2c6c67cf69977c
Akhil Mohan <132460518+akhilmohan123@users.noreply.github.com>
2024-09-29 09:59:49 +0530

    Delete Backend/HospitalManagement directory

--- Backend/HospitalManagement/patient/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from . views import PatientProfileCreateUpdateView,GetprofilePatient,GetAllDoctors
-urlpatterns = [
-   path('addprofile',PatientProfileCreateUpdateView.as_view(),name='addprofile'),
-   path('getprofile',GetprofilePatient.as_view(),name='getprofile'),
-   path('getalldoctors',GetAllDoctors.as_view(),name='getalldoctors')
-]
--- Backend/HospitalManagement/videocall/urls.py
+++
@@ -1,6 +0,0 @@
-from django.urls import path
-from .views import Video_Call_View
-
-urlpatterns=[
-    path('videocall',Video_Call_View,name='videocall')
-]
